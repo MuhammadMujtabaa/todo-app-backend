46822e4ad879dfd9ea1fcfecf7ef059f782cc990
MuhammadMujtaba <muhammadmujtaba365@gmail.com>
2024-08-04 12:44:59 +0500

    chore: routes for profile

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authRouter } from "./auth/index.mjs";
 import { userRouter } from "./users/index.mjs";
 import { todoRouter } from "./todo/index.mjs";
-
+import { profileRouter } from "./profile/index.mjs";
 export const router = Router();
 
 // Use auth router for authentication-related routes
@@ -10,7 +10,9 @@ router.use("/auth", authRouter);
 
 // Use user router for user-related routes
 router.use("/user", userRouter);
-router.use("/todo",todoRouter)
+router.use("/todo", todoRouter);
+router.use("/profile", profileRouter);
+
 // Handle unknown routes
 router.use((req, res, next) => {
   res.status(404).json({ error: "Route Not Found!" });
